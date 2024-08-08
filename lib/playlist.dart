9897028b963f8c0249de674c8d10fbadef9a8b03
Samuel Mediani <66485729+SamMed05@users.noreply.github.com>
2024-08-08 20:52:02 +0200

    Make BPM editable and allow changing song by tapping on song item from playlist

--- lib/playlist.dart
+++ lib/playlist.dart
@@ -197,6 +197,8 @@ class _PlaylistPage extends State<Playlist> {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
             content: Text('Imported song ' + fileName),
+            backgroundColor: Theme.of(context).colorScheme.primary.withOpacity(0.8),
+            duration: const Duration(seconds: 2),
           ),
         );
       }
@@ -204,6 +206,8 @@ class _PlaylistPage extends State<Playlist> {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
           content: const Text('No song chosen'),
+          backgroundColor: Theme.of(context).colorScheme.primary.withOpacity(0.8),
+          duration: const Duration(seconds: 2),
         ),
       );
     }
@@ -220,7 +224,12 @@ class _PlaylistPage extends State<Playlist> {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('Enter BPM for ${song.songName}'),
+          title: Text(
+            'Enter BPM for ${song.songName}',
+            style: TextStyle(
+              fontSize: 20,
+            )
+          ),
           content: TextField(
             controller: bpmController,
             keyboardType: TextInputType.number,
@@ -296,80 +305,87 @@ class _PlaylistPage extends State<Playlist> {
               ),
               Expanded(
                 flex: 1,
-                child: Container(
-                  margin: EdgeInsets.all(0),
-                  padding: EdgeInsets.all(6),
-                  width: 180,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(41, 128, 128, 128),
-                    shape: BoxShape.rectangle,
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(color: Color(0x4d9e9e9e), width: 0),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Color(0x1f000000),
-                          shape: BoxShape.rectangle,
-                          borderRadius: BorderRadius.circular(10),
-                          border: Border.all(color: Color(0x4d9e9e9e), width: 0),
-                        ),
-                        child: ClipRRect(
-                          // Make rounded corners in the album cover images
-                          borderRadius: BorderRadius.circular(8),
-                          child: Image(
-                            image: AssetImage(playlistProvider.playlist[index].coverImage),
-                            height: 50,
-                            width: 50,
-                            fit: BoxFit.cover,
+                child: InkWell( // Better than GestureDetector
+                  onTap: () {
+                    // Play the song at the given index
+                    goToSong(index);
+                  },
+                  child: Container(
+                    margin: EdgeInsets.all(0),
+                    padding: EdgeInsets.all(6),
+                    width: 180,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(41, 128, 128, 128),
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Color(0x4d9e9e9e), width: 0),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Color(0x1f000000),
+                            shape: BoxShape.rectangle,
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(color: Color(0x4d9e9e9e), width: 0),
+                          ),
+                          child: ClipRRect(
+                            // Make rounded corners in the album cover images
+                            borderRadius: BorderRadius.circular(8),
+                            child: Image(
+                              image: AssetImage(playlistProvider.playlist[index].coverImage),
+                              height: 50,
+                              width: 50,
+                              fit: BoxFit.cover,
+                            ),
                           ),
                         ),
-                      ),
-                    SizedBox(width: 2), // Add some spacing between image and text
-                      Expanded(
-                        flex: 1,
-                        child: Padding(
-                          padding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisSize: MainAxisSize.max,
-                            children: [
-                              Text(
-                                playlistProvider.playlist[index].songName,
-                                textAlign: TextAlign.start,
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w500,
-                                  fontStyle: FontStyle.normal,
-                                  fontSize: 14,
-                                  // color: Color(0xff000000),
+                      SizedBox(width: 2), // Add some spacing between image and text
+                        Expanded(
+                          flex: 1,
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                Text(
+                                  playlistProvider.playlist[index].songName,
+                                  textAlign: TextAlign.start,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontStyle: FontStyle.normal,
+                                    fontSize: 14,
+                                    // color: Color(0xff000000),
+                                  ),
                                 ),
-                              ),
-                              Text(
-                                playlistProvider.playlist[index].artistName,
-                                textAlign: TextAlign.start,
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w400,
-                                  fontStyle: FontStyle.normal,
-                                  fontSize: 13,
-                                  // color: Color(0xff000000),
+                                Text(
+                                  playlistProvider.playlist[index].artistName,
+                                  textAlign: TextAlign.start,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w400,
+                                    fontStyle: FontStyle.normal,
+                                    fontSize: 13,
+                                    // color: Color(0xff000000),
+                                  ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
+              // BPM number
               Container(
                 alignment: Alignment.center,
                 margin: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
@@ -383,14 +399,20 @@ class _PlaylistPage extends State<Playlist> {
                   border: Border.all(color: Color(0x4d9e9e9e), width: 0),
                 ),
                 // TODO Make BPM tappable with GestureDetector (https://api.flutter.dev/flutter/widgets/GestureDetector-class.html)
-                child: Text(
-                  playlistProvider.playlist[index].BPM.toString(),
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontWeight: FontWeight.w700,
-                    fontStyle: FontStyle.normal,
-                    fontSize: 16,
-                    // color: Color(0xff000000),
+                child: InkWell(
+                  onTap: () async {
+                    // Show dialog to edit BPM
+                    await _showEditBPMDialog(index);
+                  },
+                  child: Text(
+                    playlistProvider.playlist[index].BPM.toString(),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.normal,
+                      fontSize: 16,
+                      // color: Color(0xff000000),
+                    ),
                   ),
                 ),
               ),
@@ -398,6 +420,50 @@ class _PlaylistPage extends State<Playlist> {
           ),
         ),
       ],
+    );
+  }
+
+  // Add this method to the _PlaylistPage class
+  Future<void> _showEditBPMDialog(int songIndex) async {
+    TextEditingController bpmController = TextEditingController(
+      text: playlistProvider.playlist[songIndex].BPM.toString(),
+    );
+
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Edit BPM'),
+          content: TextField(
+            controller: bpmController,
+            keyboardType: TextInputType.number,
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Save'),
+              onPressed: () {
+                // Validate input (ensure it's a number)
+                double? newBPM = double.tryParse(bpmController.text);
+                if (newBPM != null) {
+                  // Update BPM in the playlist
+                  playlistProvider.playlist[songIndex].BPM = newBPM;
+                  playlistProvider.notifyListeners(); // Notify listeners
+                  Navigator.of(context).pop();
+                } else {
+                  // Show error message or handle invalid input
+                  // ...
+                }
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
